e9cc5a7ac9e91fff8b349f1f55b8a3288ec2e05a
daveCodeRaccoon <david.asiamah@coderaccoon.com>
2025-04-30 14:03:01 +0000

    added prettierrc

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,20 +1,18 @@
-"use client";
+'use client'
 
-import CourseInfoCard from "@/components/ui/card/course-info-card";
-import FolderCard from "@/components/ui/card/course-folder-card";
-import React from "react";
-import { folders } from "@/data/folders-card-data";
-import { useViewMode } from "@/context/view-mode-context";
-import CourseCard from "@/components/ui/card/course-card";
+import CourseInfoCard from '@/components/ui/card/course-info-card'
+import FolderCard from '@/components/ui/card/course-folder-card'
+import React from 'react'
+import { folders } from '@/data/folders-card-data'
+import { useViewMode } from '@/context/view-mode-context'
+import CourseCard from '@/components/ui/card/course-card'
 
 export default function CoursesPage() {
-  const { viewMode } = useViewMode();
+  const { viewMode } = useViewMode()
 
   return (
     <div className="flex flex-col gap-10 w-full !mt-8 ">
-      <h2 className="text-[14px] font-semibold tracking-wide text-secondary pb-[18px]">
-        Folders
-      </h2>
+      <h2 className="text-[14px] font-semibold tracking-wide text-secondary pb-[18px]">Folders</h2>
       <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[30px]">
         {folders.map((folder, idx) => (
           <FolderCard
@@ -24,24 +22,14 @@ export default function CoursesPage() {
             onClick={() => console.log(folder)}
           />
         ))}
-        <FolderCard
-          title="New folder"
-          variant="new"
-          onClick={() => console.log("Create folder")}
-        />
+        <FolderCard title="New folder" variant="new" onClick={() => console.log('Create folder')} />
       </section>
 
-      <h2 className="text-[14px] font-semibold tracking-wide text-secondary">
-        Content
-      </h2>
-      <section
-        className={`flex ${
-          viewMode === "grid" ? "flex-row" : "flex-col"
-        } gap-4`}
-      >
-        {[1, 2, 3].map((_, i) => (
+      <h2 className="text-[14px] font-semibold tracking-wide text-secondary">Content</h2>
+      <section className={`flex ${viewMode === 'grid' ? 'flex-row' : 'flex-col'} gap-4`}>
+        {[1, 2, 3, 4, 5, 6].map((_, i) => (
           <div key={i}>
-            {viewMode === "grid" ? (
+            {viewMode === 'grid' ? (
               <>
                 <CourseCard
                   title="Time management and Impact on Work"
@@ -69,5 +57,5 @@ export default function CoursesPage() {
         ))}
       </section>
     </div>
-  );
+  )
 }
--- src/app/shared/layout.tsx
+++ src/app/shared/layout.tsx
@@ -1,14 +1,14 @@
 // app/shared/layout.tsx
-"use client";
+'use client'
 
-import { ReactNode } from "react";
-import SidebarMenu from "@/components/layout/sidebar";
-import { Providers } from "../provider";
-import "../../styles/globals.css";
-import MobileViewHeader from "@/components/layout/header/mobile-view-header";
-import TabNavigator from "@/components/layout/tab-navigation";
-import { ViewModeProvider } from "@/context/view-mode-context";
-import Header from "@/components/layout/header/header";
+import { ReactNode } from 'react'
+import SidebarMenu from '@/components/layout/sidebar'
+import { Providers } from '../provider'
+import '../../styles/globals.css'
+import MobileViewHeader from '@/components/layout/header/mobile-view-header'
+import TabNavigator from '@/components/layout/tab-navigation'
+import { ViewModeProvider } from '@/context/view-mode-context'
+import Header from '@/components/layout/header/header'
 
 export default function SharedLayout({ children }: { children: ReactNode }) {
   return (
@@ -24,9 +24,7 @@ export default function SharedLayout({ children }: { children: ReactNode }) {
           {/* Main Content */}
           <div className="bg-[#181818] flex flex-col h-screen px-5 ">
             <Header />
-            <div className="flex flex-col overflow-y-auto no-scrollbar">
-              {children}
-            </div>
+            <div className="flex flex-col overflow-y-auto no-scrollbar">{children}</div>
             <div className="md:hidden fixed bottom-0 left-0 w-full  flex justify-around items-center h-[90px] bg-[#181818]">
               <TabNavigator />
             </div>
@@ -34,5 +32,5 @@ export default function SharedLayout({ children }: { children: ReactNode }) {
         </div>
       </ViewModeProvider>
     </Providers>
-  );
+  )
 }
