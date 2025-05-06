11d9a8a49b9467348c8c4aa5aac71f6e407145c4
Desmond Essuman <desmond.essuman@coderaccoon.com>
2025-05-06 11:36:43 +0000

    fix(text-color): text color scheme for both dark and light mode

--- src/components/ui/card/course-folder-card.tsx
+++ src/components/ui/card/course-folder-card.tsx
@@ -21,17 +21,17 @@ const FolderCard: React.FC<FolderCardProps> = ({
     <div
       onClick={onClick}
       className={`flex items-center justify-between flex-wrap sm:flex-nowrap w-full rounded-2xl bg-[#222222] px-[26px] sm:px-6 py-[18px] cursor-pointer transition-colors
-        ${isNewFolder ? "text-primary hover:bg-[#2a2a2a]" : "text-secondary hover:bg-[#2a2a2a]"}`}
+        ${isNewFolder ? "text-primary hover:bg-[#2a2a2a]" : "text-text-secondary hover:bg-[#2a2a2a]"}`}
     >
       <div className="flex items-center gap-[23px] sm:gap-6 min-w-0">
         {isNewFolder ? (
           <PlusIcon className="h-6 w-6 text-primary shrink-0" />
         ) : (
-          <FolderIcon className="h-6 w-6 text-secondary shrink-0" />
+          <FolderIcon className="h-6 w-6 text-text-secondary  shrink-0" />
         )}
         <span
           className={`font-['comfortaa'] font-semibold text-[14px] leading-none tracking-normal truncate
-            ${isNewFolder ? "text-primary" : "text-secondary"}`}
+            ${isNewFolder ? "text-primary" : "text-text-secondary "}`}
         >
           {title}
         </span>
