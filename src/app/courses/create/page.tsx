e992a5bb38ae3eee13aacf442b4193a17af1cc05
mary <mary.jimoh@coderaccoon.com>
2025-05-13 18:25:53 +0000

    refactor:moved item into seperate component

--- src/app/courses/create/page.tsx
+++ src/app/courses/create/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import CoursesBuilder from '@/features/courses/course-builder'
+import CoursesBuilder from '@/features/courses/course_builder/course-builder'
 
 export default function CoursesPage() {
   return <CoursesBuilder />
