f2d2eda91f349fa3cbeffd074272c9f0d1fd017d
Agbekey <richard.agbekey@dicebridgegmbh.onmicrosoft.com>
2025-05-19 09:50:48 +0000

    text format sidebar added

---
+++ src/features/courses/components/course_creation/text-format.tsx
@@ -0,0 +1,22 @@
+
+import { TEXT_BLOCKS } from './blocks/text_blocks/text-block-data'
+
+
+export default function TextFormats() {
+  return (
+    <div className="absolute inset-y-0 left-0 bg-[#282828] w-[20%] ml-[15%] h-full overflow-y-scroll no-scrollbar">
+      <div className="first p-8 ">
+        {TEXT_BLOCKS.map(({type,content}) => (
+          <div className='first:mt-0 mt-7' key={type}>
+            <p className="mb-2.5 text-xs">{type}</p>
+            <div className="py-7 px-6 bg-[#444444] rounded-md">
+              <p className="text-[0.5rem] text-left">
+              {content}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
